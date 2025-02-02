ae94c30ea52d248693f2f6c15cca94075227130d
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-02 19:07:39 +0100

    Change CORS

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,18 +1,23 @@
 import express from 'express';
-
-import { logDate } from '../middlewares/logger-middleware';
 import errorHandler from '../middlewares/error-handler';
 import router from '../routes/index';
-
-import config from '../config';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { connectDatabase } from '../services/createConnection';
+import { logDate } from '../middlewares/logger-middleware.js';
 
 export default async function (server: any) {
+  const allowedOrigins = ['https://blog.mapach.es'];
+
   server.use(
     cors({
-      origin: 'https://blog.mapach.es',
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
       credentials: true,
       allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     })
@@ -33,8 +38,4 @@ export default async function (server: any) {
   });
 
   server.use(errorHandler);
-
-  server.listen(config.port, () => {
-    console.log(`Server listening on port: ${config.port}`);
-  });
 }
