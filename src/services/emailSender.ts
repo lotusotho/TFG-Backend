3936464e3a7d60f06c2a5361898772d9a41a0a2d
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-13 22:01:13 +0100

    fix __dirname not found

--- src/services/emailSender.ts
+++ src/services/emailSender.ts
@@ -4,6 +4,10 @@ import { emailConfig } from '../config.js';
 import handlebars from 'handlebars';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const mailgunOptions = {
   auth: {
