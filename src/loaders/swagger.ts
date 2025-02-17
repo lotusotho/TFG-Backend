ecd68392cc41831fd23c76997cde32a5a1bcb051
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-17 21:14:42 +0100

    dirname not defined

--- src/loaders/swagger.ts
+++ src/loaders/swagger.ts
@@ -2,6 +2,10 @@ import { Express } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
 
