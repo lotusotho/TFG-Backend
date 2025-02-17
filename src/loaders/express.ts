a7e1be532959bac5130a71406a4182ea67c98be8
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-17 21:18:25 +0100

    Change swagger loader

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-
 import { logDate } from '../middlewares/logger-middleware';
 import errorHandler from '../middlewares/error-handler';
 import router from '../routes/index';
-
 import config from '../config';
 import cors from 'cors';
 import { connectDatabase } from '../services/createConnection';
+import swaggerLoader from './swagger.js';
 
 export default async function (server: any) {
   server.use(
@@ -22,6 +21,9 @@ export default async function (server: any) {
   server.use(express.urlencoded({ extended: true }));
 
   server.use(logDate);
+
+  swaggerLoader(server);
+
   server.use(router);
 
   await connectDatabase();
