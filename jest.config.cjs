9231c6bf544dd826dd4406db30fb8086e0e8421b
Alejandro Ramos <lotusotho11@gmail.com>
2025-03-04 20:41:16 +0100

    Remove scanner work

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: ['src/controllers/*.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
 };
